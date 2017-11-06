fb962a6c2558f649081b71383934ac19c8e312b7
WaiCheong <33353343+WaiCheong@users.noreply.github.com>
2017-11-06 15:26:11 +0100

    spelling

--- Login/login.php
+++ Login/login.php
@@ -17,7 +17,7 @@
         <a href="contact.php">Contact</a>
         <a href="login2.php">Inloggen</a>
 </div>
-<span class="first_tittle"><h3>Kom Essen, Lekker en snel geleverd!</h3></span>
+<span class="first_title"><h3>Kom Essen, Lekker en snel geleverd!</h3></span>
 
 <div id="left_sidebar">
 <div id="menu">
